ab95f8baa249651337468e54df92e9614e0221c8
Zach Goldberg <zach@equi.com>
2023-12-21 11:56:17 -0800

    looks like old repo is unmaintained, so republish with new maintainer

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-! This extension will no longer be developed !
 # Battery Consumption Watt Meter
 
 https://extensions.gnome.org/extension/4862/bat_consumption_wattmeter/
@@ -21,6 +20,7 @@ TO DO: <br>
 <strike>1. Settings - custom reload interval</strike> **DONE** <br>
 <strike>2. Settings - ON/OFF consumption info when battery is charging</strike> **DONE** <br>
 <strike>3. Settings - ON/OFF battery percent</strike> **DONE** <br>
-4. Color? warning when consumption exceeds certain value (f.e. -40W) <br>
-5. Tell me...
+<strike>4. Settings - ON/OFF battery time remaining</strike> **DONE** <br>
+5. Color? warning when consumption exceeds certain value (f.e. -40W) <br>
+6. Tell me...
 
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo 'pack'
-rm batt_consumption_wattmetter@wennaspeedy.shell-extension.zip
+rm batt_consumption_wattmetter@zachgoldberg.shell-extension.zip
 gnome-extensions pack --force --extra-source=utils.js
 echo 'uninstall'
-#gnome-extensions uninstall batt_consumption_wattmetter@wennaspeedy || true
+gnome-extensions uninstall batt_consumption_wattmetter@zachgoldberg || true
 echo 'install'
-gnome-extensions disable batt_consumption_wattmetter@wennaspeedy
-gnome-extensions install --force batt_consumption_wattmetter@wennaspeedy.shell-extension.zip
-gnome-extensions reset batt_consumption_wattmetter@wennaspeedy
-gnome-extensions enable batt_consumption_wattmetter@wennaspeedy
+gnome-extensions disable batt_consumption_wattmetter@zachgoldberg
+gnome-extensions install --force batt_consumption_wattmetter@zachgoldberg.shell-extension.zip
+gnome-extensions reset batt_consumption_wattmetter@zachgoldberg
+gnome-extensions enable batt_consumption_wattmetter@zachgoldberg
